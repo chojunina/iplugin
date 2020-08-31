ecf55855a277af88f8cdd9f30f53d9fdeb99bb30
Raina <jhcho1102@naver.com>
2020-08-31 21:24:59 +0900

    Add files via upload

---
+++ src/com/jh/test/Main.java
@@ -0,0 +1,28 @@
+package com.jh.test;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    public Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        instance.getLogger().info("이나의 플러그인이 시작 됩니다");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onLoad() {
+        instance = this;
+        instance.getLogger().info("이나의 플러그인이 멈춤니다");
+    }
+
+    public void PlayerJoin(PlayerJoinEvent event){
+        event.getPlayer().sendMessage("서버에 오신걸 환영합니다");
+    }
+
+}
